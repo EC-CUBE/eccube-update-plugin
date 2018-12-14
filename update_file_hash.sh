a60d43418275e45646e15d6be94bc613d69539a6
Chihiro Adachi <8196725+chihiro-adachi@users.noreply.github.com>
2018-12-14 10:35:33 +0900

    ハッシュ生成スクリプトを更新

--- update_file_hash.sh
+++ update_file_hash.sh
@@ -4,6 +4,11 @@ if [ -d ec-cube ]
 then
     rm -rf ec-cube
 fi
+
+echo '' > update_files.txt
+echo '' > file_hash.yaml
+echo '' > file_hash_crlf.yaml
+
 mkdir ec-cube
 cd ec-cube
 curl http://downloads.ec-cube.net/src/eccube-4.0.0.tar.gz | tar xz --strip-components 1
@@ -11,11 +16,15 @@ git init .
 git add .
 git commit -m 'first commit'
 curl http://downloads.ec-cube.net/src/eccube-4.0.1.tar.gz | tar xz --strip-components 1
-git diff --name-only > update_files.txt
+git add .
+git diff --name-only --cached > ../update_files.txt
 git reset --hard HEAD
 while read file
 do
     md5 $file | sed -e "s/MD5 (//" -e "s/) = /: /" >> ../file_hash.yaml
     perl -p -i -e 's/\n/\r\n/g' $file
     md5 $file | sed -e "s/MD5 (//" -e "s/) = /: /" >> ../file_hash_crlf.yaml
-done < update_files.txt
+done < ../update_files.txt
+
+cd ..
+rm -rf ec-cube
