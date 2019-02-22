794d32187a0de588cbeac28f66ae8faa05c59ec2
Chihiro Adachi <8196725+chihiro-adachi@users.noreply.github.com>
2019-02-22 15:13:22 +0900

    4.0.1 -> 4.0.2 のアップデートプラグインの初版作成

---
+++ Common/Constant.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * This file is part of EC-CUBE
+ *
+ * Copyright(c) LOCKON CO.,LTD. All Rights Reserved.
+ *
+ * http://www.lockon.co.jp/
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Plugin\EccubeUpdater401to402\Common;
+
+class Constant
+{
+    const FROM_VERSION = '4.0.1';
+    const TO_VERSION = '4.0.2';
+    const PLUGIN_CODE = 'EccubeUpdater401to402';
+}
